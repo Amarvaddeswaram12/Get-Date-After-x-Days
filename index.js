c0a6cc8ca388a997c661ff821eca7bac20316599
Amarvaddeswaram12 <amar.vaddeswaram12@gmail.com>
2022-11-13 09:56:50 +0000

    get date after x days

---
+++ index.js
@@ -0,0 +1,11 @@
+const addDays = require("date-fns/addDays");
+
+const getDaysAfterXdays = (days) => {
+  const addDays = require("date-fns/addDays");
+
+  return `${newDate.getDate()} - ${
+    newDate.getMonth() + 1
+  } - ${newDate.getFullYear()}`;
+};
+
+module.exports = getDaysAfterXdays;
